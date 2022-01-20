83aaa9fbafed7aecc543a35e8ad382e24b799cd5
aronei44 <meliodasmeliodas224@gmail.com>
2022-01-20 14:47:09 +0700

    feat cart: add realtime in cart feature #1

--- context/CartContext.js
+++ context/CartContext.js
@@ -1,3 +1,4 @@
+import callAPI from '@/config/api';
 import React, { createContext, useContext, useReducer } from 'react';
 import toast from 'react-hot-toast';
 
@@ -6,8 +7,9 @@ export const CartContext = createContext();
 // export const CartProvider = (props) => {}
 
 const initialState = {
-  cart: {},
-  totalPrice: 0,
+  cart: [],
+  buff: false,
+  get: true,
 };
 
 export const useCartContext = () => {
@@ -22,54 +24,18 @@ export const useCartContext = () => {
 
 const Reducer = (state, action) => {
   switch (action.type) {
-    case 'ADD_TO_CART':
-      if (state.cart[action.item.product.id]?.product.id !== action.item.product.id) {
-        toast.success(`Produk Berhasil Dimasukan Ke Keranjang`);
-      } else {
-        toast.success(`Produk Sudah Ada Di Keranjang`);
-      }
-
+    case 'GET_CARTS':
       return {
         ...state,
-        cart: state.cart
-          ? {
-              ...state.cart,
-              [action.item.product.id]: action.item,
-            }
-          : { [action.item.product.id]: action.item },
+        cart: action.payload,
       };
 
-    case 'REMOVE_FROM_CART':
-      toast.success(`Produk Berhasil Dihapus Dari Keranjang`);
-
-      return {
-        ...state,
-        cart: Object.keys(state.cart)
-          .filter((key) => +key !== +action.id)
-          .reduce((acc, key) => {
-            const item = state.cart[key];
-            // console.log(item);
-            acc[item.product.id] = item;
-            // console.info(acc[item.product.id]);
-            return acc;
-          }, {}),
-      };
-
-    case 'RESET_CART':
-      return {
-        ...state,
-        cart: initialState.cart,
-        totalPrice: initialState.totalPrice,
-      };
-
-    default: {
-      throw new Error(`Unhandled action type ${action.type}`);
-    }
+    default:
+      console.log('Awokwokwkwok');
   }
 };
 
-export default function Provider(props) {
+export default function CartProvider(props) {
   const [state, dispatch] = useReducer(Reducer, initialState);
-  console.info(state);
   return <CartContext.Provider value={[state, dispatch]} {...props} />;
 }
