2227d9ea75cd0efc9b87b925b0b0a66fd4e893af
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-01-26 14:12:21 +0700

    feat: order list pembeli

--- components/templates/UserSettingsLayout.jsx
+++ components/templates/UserSettingsLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Tab } from '@headlessui/react';
 import { HiOutlineUserCircle, HiOutlineKey } from 'react-icons/hi';
-import { GrMapLocation } from 'react-icons/gr';
+import { GrMapLocation, GrTransaction } from 'react-icons/gr';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -13,41 +13,47 @@ import Link from 'next/link';
 const UserSettingsLayout = ({ children }) => {
   return (
     <Main className='relative min-h-screen mb-6'>
-      <div className='relative max-w-7xl px-4 pt-20 mx-auto sm:px-6 lg:px-8'>
+      <div className='relative px-4 pt-20 mx-auto max-w-7xl sm:px-6 lg:px-8'>
         <div className='grid grid-cols-4 gap-6'>
           <div>
             <div className='p-5 border border-gray-200 rounded-lg shadow-sm'>
               <div className='flex items-center justify-between'>
                 <h3 className='text-lg font-medium leading-6 text-gray-900'>User Profile</h3>
               </div>
-              <div className='flex items-center justify-between pt-4 border-b border-gray-200 pb-4'>
+              <div className='flex items-center justify-between pt-4 pb-4 border-b border-gray-200'>
                 <div className='flex items-center'>
                   <div className='avatar placeholder'>
-                    <div className='bg-neutral-focus text-neutral-content rounded-full w-12 h-12'>
+                    <div className='w-12 h-12 rounded-full bg-neutral-focus text-neutral-content'>
                       <span className='text-lg'>FM</span>
                     </div>
                   </div>
                   <div className='ml-3'>
-                    <div className='text-sm leading-5 font-medium text-gray-700'>Farhan Mubarok</div>
+                    <div className='text-sm font-medium leading-5 text-gray-700'>Farhan Mubarok</div>
                   </div>
                 </div>
               </div>
               <Link href={'/profile/settings'}>
-                <a className='hover:bg-gray-100 rounded-lg h-12 px-2 w-full text-gray-800 dark:text-white flex items-center p-2 mt-2 transition-colors duration-200 justify-start'>
-                  <span className='text-left text-xl text-gray-700'>
+                <a className='flex items-center justify-start w-full h-12 p-2 px-2 mt-2 text-gray-800 transition-colors duration-200 rounded-lg hover:bg-gray-100 dark:text-white'>
+                  <span className='text-xl text-left text-gray-700'>
                     <HiOutlineUserCircle />
                   </span>
-                  <span className='mx-2 pl-2 text-gray-700 text-sm font-medium'>Biodata Diri</span>
+                  <span className='pl-2 mx-2 text-sm font-medium text-gray-700'>Biodata Diri</span>
                 </a>
               </Link>
               <Link href={'/profile/settings/address'}>
-                <a
-                  className='hover:bg-gray-100 rounded-lg h-12 px-2 w-full text-gray-800 dark:text-white flex items-center p-2 transition-colors duration-200 justify-start'
-                  href='#'>
-                  <span className='text-left text-xl text-gray-700'>
+                <a className='flex items-center justify-start w-full h-12 p-2 px-2 text-gray-800 transition-colors duration-200 rounded-lg hover:bg-gray-100 dark:text-white'>
+                  <span className='text-xl text-left text-gray-700'>
                     <GrMapLocation />
                   </span>
-                  <span className='mx-2 pl-2 text-gray-700 text-sm font-medium'>Daftar Alamat</span>
+                  <span className='pl-2 mx-2 text-sm font-medium text-gray-700'>Daftar Alamat</span>
+                </a>
+              </Link>
+              <Link href={'/profile/order-list'}>
+                <a className='flex items-center justify-start w-full h-12 p-2 px-2 text-gray-800 transition-colors duration-200 rounded-lg hover:bg-gray-100 dark:text-white'>
+                  <span className='text-xl text-left text-gray-700'>
+                    <GrTransaction />
+                  </span>
+                  <span className='pl-2 mx-2 text-sm font-medium text-gray-700'>Daftar Transaksi</span>
                 </a>
               </Link>
             </div>
